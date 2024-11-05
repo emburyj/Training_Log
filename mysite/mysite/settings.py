7e0159fb7b90e447f3e4407d743bd4dab45207c7
Josh <joshembury@gmail.com>
2024-11-04 22:08:15 -0700

    ready for milestone 1 submission

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 LOGIN_URL = 'Login/'
 LOGIN_REDIRECT_URL = '/Training-Log/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
