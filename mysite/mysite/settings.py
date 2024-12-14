1ba9c6851fb9142c958feee562d63b42e373c12a
Josh <joshembury@gmail.com>
2024-12-14 10:47:23 -0700

    updated setting for prod

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
